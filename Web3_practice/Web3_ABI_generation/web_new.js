e6e5d7b9328ef582413869f61502cfa470aa3f90
Akash <akashghode1234@gmail.com>
2022-10-23 18:14:06 +0530

    deployment

--- Web3_practice/Web3_ABI_generation/web_new.js
+++ Web3_practice/Web3_ABI_generation/web_new.js
@@ -35,3 +35,25 @@ ABI = output.contracts["demo.sol"]["demo"].abi;
 bytecode = output.contracts["demo.sol"]["demo"].evm.bytecode.object;
 console.log("abi:", ABI);
 console.log("bytecode:", bytecode);
+
+
+contract = new web3.eth.Contract(ABI);
+let defaultAccount;
+web3.eth.getAccounts().then((accounts) => {
+  console.log("Accounts:", accounts); // all the ganache accounts
+
+  defaultAccount = accounts[0];
+  console.log("Default Account:", defaultAccount);  //to deploy the contract from default Account
+  contract
+    .deploy({ data: bytecode })
+    .send({ from: defaultAccount, gas: 470000 })
+    .on("receipt", (receipt) => { //event,transactions,contract address will be returned by blockchain
+      console.log("Contract Address:", receipt.contractAddress);
+    })
+    .then((demoContract) => {
+      demoContract.methods.x().call((err, data) => {
+        console.log("Initial Value:", data);
+      });
+    });
+  
+});
